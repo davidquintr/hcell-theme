f3686d10b02ce57ae06feffa966753002d5df528
davidquintr <davidquintr97@gmail.com>
2024-12-14 01:05:51 -0600

    add styles for floating menu

--- blocks/floating-menu/floating.php
+++ blocks/floating-menu/floating.php
@@ -1,10 +1,16 @@
 <?php 
     $social_medias = get_field('social_medias', 'option');
-    $show_floating_menu = get_field("show_floating_menu", "option")
+    $show_floating_menu = get_field("show_floating_menu", "option");
+    
+    $visuals_floating_menu = get_field("visuals_floating_menu", "option");
+
+    $show_tag_on_hover = $visuals_floating_menu["show_tag_on_hover"];
+    $menu_location = $visuals_floating_menu["menu_location"];
+
 ?>
 
 <?php if($social_medias): ?>
-    <ul class="tecnoblock-floating-menu">
+    <ul class="tecnoblock-floating-menu <?php echo 'location-' . $menu_location  ?>">
         <?php foreach($social_medias as $media): 
             $media_info = $media["social_media"];
             $media_label = $media_info["label"];
@@ -13,10 +19,12 @@
             $media_link = $media["link"];
         ?>
             <li class="tecnoblock-floating-menu-item">
-                <a href="<?php echo $media_link ?>">
+                <a target="_blank" class="tecnoblock-floating-menu-item-icon" href="<?php echo $media_link ?>">
                     <i class="<?php echo $media_value ?>"></i>
                 </a>
-                <span><?php echo $media_label ?></span>
+                <div class="label <?php echo 'location-' . $menu_location  ?>">
+                    <span ><?php echo $media_label ?></span>
+                </div>
             </li>
         <?php endforeach ?>
     </ul>
